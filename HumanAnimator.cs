b6a63b95e268614b170e4581de9387077976d5e5
savva_9 <148879501+savva9@users.noreply.github.com>
2025-02-20 16:09:18 +0300

    Add files via upload

---
+++ HumanAnimator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class HumanAnimator : MonoBehaviour
+{
+    public NavMeshAgent agent; // Агент
+    public Animator animator; // Аниматор
+
+    private bool canMove = true;
+
+    private void Start()
+    {
+        agent = agent.GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        // Проверка на достижение цели
+        if (agent.remainingDistance <= agent.stoppingDistance && this.GetComponent<NPCAI>().pickUpStage != 5)
+        {
+            StopMovement(16);
+        }
+    }
+
+    // Универсальная функция для перемещения к объекту
+    public void MoveToObject(GameObject targetObject, bool run = false)
+    {
+        // Анимация движения
+        animator.SetInteger("arms", 1);
+        animator.SetInteger("legs", 1);
+    }
+
+    private void StopMovement(int hand) // 4 16
+    {
+        animator.SetInteger("arms", 16);
+        animator.SetInteger("legs", 0);
+    }
+}
